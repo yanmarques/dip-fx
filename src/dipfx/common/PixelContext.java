b4973bbaa8f4b3df72af078eb1b7ecf4e788c90f
Yan Marques de Cerqueira <marques_yan@outlook.com>
2020-04-04 23:20:13 -0300

    Refactor pixel toolkit

--- src/dipfx/common/PixelContext.java
+++ src/dipfx/common/PixelContext.java
@@ -1,5 +1,6 @@
 package dipfx.common;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
@@ -8,16 +9,36 @@ import javafx.scene.paint.Color;
 public class PixelContext {
     private int axisX;
     private int axisY;
+    private Image originalImage;
+    private Integer[] geometry;
     private PixelReader pixelReader;
     private WritableImage image;
     private PixelWriter pixelWriter;
 
-    public PixelContext(int axisX, int axisY, PixelReader pixelReader, WritableImage image, PixelWriter pixelWriter) {
+    public PixelContext(Image image, int axisX, int axisY) {
         this.axisX = axisX;
         this.axisY = axisY;
-        this.pixelReader = pixelReader;
-        this.image = image;
-        this.pixelWriter = pixelWriter;
+        this.originalImage = image;
+
+        int height = (int) image.getHeight();
+        int width = (int) image.getWidth();
+
+        this.geometry = new Integer[]{width, height};
+        this.pixelReader = image.getPixelReader();
+        this.image = new WritableImage(width, height);
+        this.pixelWriter = this.image.getPixelWriter();
+    }
+
+    public PixelContext(Image image) {
+        this(image, -1, -1);
+    }
+
+    public Image getOriginalImage() {
+        return originalImage;
+    }
+
+    public Integer[] getGeometry() {
+        return geometry;
     }
 
     public int getAxisX() {
--- src/dipfx/common/PixelToolkit.java
+++ src/dipfx/common/PixelToolkit.java
@@ -14,32 +14,38 @@ public class PixelToolkit {
     private static final Logger logger = LogUtil.getLogger(PixelToolkit.class.getName());
 
     private Callback<PixelContext, Color> runner;
+    private int initialPixel;
+    private int untilWidth;
+    private int untilHeight;
+
+    public PixelToolkit(Callback<PixelContext, Color> runner, int initialPixel, int untilWidth, int untilHeight) {
+        this.runner = runner;
+        this.initialPixel = initialPixel;
+        this.untilWidth = untilWidth;
+        this.untilHeight = untilHeight;
+    }
 
     public PixelToolkit(Callback<PixelContext, Color> runner) {
-        this.runner = runner;
+        this(runner, 0, 0, 0);
     }
 
     public Image apply(Image image) {
-        int height = (int) image.getHeight();
-        int width = (int) image.getWidth();
+        PixelContext context = new PixelContext(image);
 
-        logger.log(Level.FINE, "got image resolution: {0}x{1}", new Object[]{width, height});
+        logger.log(Level.FINE, "got image resolution: {0}x{1}", context.getGeometry());
 
-        PixelReader pR = image.getPixelReader();
-        WritableImage wI = new WritableImage(width, height);
-        PixelWriter pW = wI.getPixelWriter();
+        int width = context.getGeometry()[0] + this.untilWidth;
+        int height = context.getGeometry()[1] + this.untilHeight;
 
-        PixelContext context = new PixelContext(-1, -1, pR, wI, pW);
-
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
+        for (int i = this.initialPixel; i < width; i++) {
+            for (int j = this.initialPixel; j < height; j++) {
                 context.setAxisX(i);
                 context.setAxisY(j);
                 Color newColor = this.runner.call(context);
-                pW.setColor(i, j, newColor);
+                context.getPixelWriter().setColor(i, j, newColor);
             }
         }
 
-        return wI;
+        return context.getImage();
     }
 }
